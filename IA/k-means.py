6bf1762494a5de0f42562432c44b32cbfcb88155
Samuel Eduardo <samueleduardo1609@hotmail.com>
2018-03-06 03:19:33 -0300

    Update k-means.py

--- IA/k-means.py
+++ IA/k-means.py
@@ -1,30 +1,96 @@
 import math
 import random
+import sys
 
-class Point:
+
+
+
+class Ponto:
     
-    x = None
-    y = None
-    z = None
-    v = None
+    cords = []
     
-    def __init__(self, x, y, z, v):
-        self.x = x
-        self.y = y
-        self.z = z
-        self.v = v
+    def __init__(self, cords, cluster = None):
+        self.cords = [x for x in cords]
+        self.grupo = cluster
+
+
+class Grupo:
+
+
+    def __init__(self, centroide, grupo, aux):
+        self.centroide = centroide
+        self.grupo = grupo
+        self.pontos = [aux]
+
+def exp(x):
+    return x*x
+
+def dist(a, b):
+
+    dist = 0
+
+    for i in range(len(a.cords)):
+
+        dist += exp(a.cords[i] - b.cords[i])
+
+    return math.sqrt(dist)
+
+
 
 #lista de pontos do modelo, K do KMeans
 def Kmeans(pontos, k, iterMax):
     
-    centroides = []
-    
+    grupos = []
+    infinito = sys.maxsize
+    n = len(pontos)
+
+    aux = [i for i in range(n)]
+    random.shuffle(aux)
+
+    aux = aux[:k]
+
     #definindo K centroides aleatorios
     
-    aux = [i for i in range(k)]
-    print aux
-    print random.shuffle(aux)
-    print aux
+    grupos = [Grupo(pontos[aux[i]], i, aux[i]) for i in range(k)]
+    
+
+    while iterMax:
+        
+        for i in range(n):
+
+            menorValor = infinito
+            centroideMaisProximo = 0
+
+            for j in range(k):
+
+                distancia = dist(pontos[i], grupos[j].centroide)
+                if(menorValor > distancia):
+                    menorValor = distancia
+                    centroideMaisProximo = j
+
+            if (i  not in grupos[centroideMaisProximo].pontos):
+
+                grupos[centroideMaisProximo].pontos.append(i)
+                pontos[i].grupo = centroideMaisProximo
+                if(pontos[i].grupo != centroideMaisProximo):
+                    grupos[centroideMaisProximo].pontos.remove(i)
+
+        #calcular novos centroides
+        
+                
+        iterMax -= 1
+
+    print("Grupos e seus pontos:")
+    
+
+    for i in range (k):
+        print('Cluster %d' %(i + 1))
+        for j in grupos[i].pontos:
+            print ("Ponto %d: " %j)
+            for l in range(len(pontos[0].cords)):
+                print(str(pontos[j].cords[l]) + " ", end = '')
+            print("")
+
     
 
 #main
@@ -35,12 +101,16 @@ disney = arq.readlines()
 
 pontos = []
 
+
+
 for linha in disney:
     x =linha.split(",")
   
     if(len(x) != 5): break
-    ponto = Point(float(x[0]) , float(x[1]), float(x[2]), float(x[3]))
+    cords = [float(x[0]) , float(x[1]), float(x[2]), float(x[3])]
+    ponto = Ponto(cords)
     pontos.append(ponto)
+    
 
 print("Digite o K para o algoritmo de KNN:")
 
@@ -51,5 +121,3 @@ print("Digite o numero maximo de iteracoes")
 iterMax = int(input())    
     
 Kmeans(pontos, k, iterMax)
-
-
