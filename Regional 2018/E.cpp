26aa99c55e05d94f950d42c889dc396608a21a73
Samuel Eduardo <samueleduardo1609@hotmail.com>
2018-09-24 21:25:33 -0300

    Add files via upload

---
+++ Regional 2018/E.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string a,b;
+
+int main()
+{
+	cin >> a >> b;
+
+	int dif = a.size() - b.size();
+	int ans = 0;
+	for(int i = 0 ; i <= dif; i++)
+	{
+		bool pegou = false;
+		for(int j = 0 ; j < b.size(); j++)
+		{
+			if(a[j + i] == b[j])
+			{
+				pegou = true;
+				break;
+			}
+		}
+		if(!pegou) ans++;
+	}
+	printf("%d\n",ans);
+
+	return 0;
+}
